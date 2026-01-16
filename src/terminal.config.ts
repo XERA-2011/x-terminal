2f4c049565ca1e4083a611b08831afc60ead4e2e
🍀 <50286230+XERA-2011@users.noreply.github.com>
2026-01-16 09:02:39 +0800

    chore: Remove '/x-terminal/' entry from aliyun configuration.

--- src/terminal.config.ts
+++ src/terminal.config.ts
@@ -7,7 +7,7 @@ export const terminalConfig = {
     name: "XERA-2011",
     email: "1138006034@qq.com",
     github: "XERA-2011",
-    aliyun: ['/x-terminal/', '/texas-holdem/', '/jupyter/lab/'],
+    aliyun: ['/texas-holdem/', '/jupyter/lab/'],
     sites: [
         {
             name: "blog",
