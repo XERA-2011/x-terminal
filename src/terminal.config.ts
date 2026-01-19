ce6436efced9ca43520ef2d591a8501edde50a3c
xera-2011 <1138006034@qq.com>
2026-01-19 19:42:22 +0800

    feat: Add `terminal.config.ts` to define customizable terminal settings and user information.

--- src/terminal.config.ts
+++ src/terminal.config.ts
@@ -7,7 +7,7 @@ export const terminalConfig = {
     name: "XERA-2011",
     email: "1138006034@qq.com",
     github: "XERA-2011",
-    aliyun: ['/texas-holdem/', '/jupyter/lab/'],
+    aliyun: ['/texas-holdem/', '/analytics/', '/jupyter/lab/'],
     sites: [
         {
             name: "blog",
