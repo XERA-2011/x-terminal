4bbab0338f4b58ff7bffbdbce93494826513a7bb
🍀 <50286230+XERA-2011@users.noreply.github.com>
2026-01-28 10:33:51 +0800

    Remove manifest link from metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,7 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.ico',
     apple: '/apple-touch-icon.png',
-  },
-  manifest: '/manifest.json',
+  }
 };
 
 export default function RootLayout({
